07a42d3efc91e8791154bb859bb20eb41c1aebee
Harish8444rgit <harishyadav8444@gmail.com>
2022-12-20 21:07:11 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Footer() {
+
+    return (
+
+        <div>
+
+            <footer className="text-light py-2" style={{ position: "scroll", backgroundColor: "#000814" }}>
+
+                <div className="footer-copyright text-center">© 2022 Copyright :
+                    <a href="https://www.linkedin.com/in/harish-yadav-3ab363241/" target="_blank" rel='noreferrer' style={{ color: "#ffc300" }} className='mx-1'>Harish Yadav</a>
+                </div>
+
+            </footer>
+        </div>
+
+    )
+
+}
